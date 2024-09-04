04611070e64f39aa7e2e8bf58a82b785e341504b
AsKiro <1720194603@qq.com>
2024-09-05 00:33:48 +0800

    获取所有用户的信息

--- Nursing_Management_front/src/views/FamilyView.vue
+++ Nursing_Management_front/src/views/FamilyView.vue
@@ -17,7 +17,7 @@
                     <el-table-column label="操作" align="center">
                         <template #default="scope">
                             <el-button size="small" type="success"
-                                @click="showUpdateDialog(scope.row.cid)">修改</el-button>
+                                @click="showUpdateDialog(scope.row.fid)">修改</el-button>
                             <el-popconfirm title="你确定要删除改家属吗?" confirm-button-text="确认" cancel-button-text="取消"
                                 @confirm="deleteFamily(scope.row.fid);">
                                 <template #reference>
@@ -35,6 +35,38 @@
             </el-card>
         </el-col>
     </el-row>
+
+    <!-- 修改客户的对话框开始 -->
+    <el-dialog v-model="updateDialogShow" title="修改客户" width="500">
+        <el-form>
+            <el-form-item label="姓名:" label-width="18%">
+                <el-input v-model="familyUpdate.fname" autocomplete="off" style="width: 300px" />
+            </el-form-item>
+            <el-form-item label="年龄:" label-width="18%">
+                <el-input v-model="familyUpdate.fage" autocomplete="off" style="width: 300px" />
+            </el-form-item>
+            <el-form-item label="性别:" label-width="18%">
+                <el-radio-group v-model="familyUpdate.fgender" style="width: 300px">
+                    <el-radio value="男" size="large">男</el-radio>
+                    <el-radio value="女" size="large">女</el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="手机号:" label-width="18%">
+                <el-input v-model="familyUpdate.fphone" autocomplete="off" style="width: 300px" />
+            </el-form-item>
+            <el-form-item label="家属:" label-width="18%">
+                <el-input v-model="familyUpdate.fname" autocomplete="off" style="width: 300px" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="updateDialogShow = false">取消</el-button>
+                <el-button type="primary">确认</el-button>
+            </div>
+        </template>
+    </el-dialog>
+    <!-- 修改客户的对话框结束 -->
+
 </template>
 
 <script setup>
@@ -43,6 +75,17 @@ import customApi from '@/api/customApi';
 import { ref } from 'vue';
 import { ElMessage } from 'element-plus';
 
+//被修改家属的信息
+const familyUpdate = ref({
+    fid: '',
+    fname: '',
+    fage: '',
+    fgender: '',
+    fphone: '',
+    cid: ''
+})
+
+
 //搜索
 const fname = ref('');
 //分页信息
@@ -50,6 +93,9 @@ const pageInfo = ref({
     total: 0,
     pageInfo: 0
 })
+
+//删除对话框显示状态
+const updateDialogShow = ref(false);
 
 //所有客户
 const customList = ref([]);
@@ -61,13 +107,40 @@ function selectByPage(pageNum) {
             pageInfo.value = resp.data;
         })
 }
+//获取所有用户的信息
+function selectByPage2(pageNum) {
+    customApi.selectByPage(pageNum)
+        .then(resp => {
+            customList.value = resp.data.list;            
+        })
+}
+
+//查询所有家属并显示修改对话框
+function showUpdateDialog(fid) {
+    customApi.selectAll()
+        .then(resp => {
+            departmentList.value = resp.data;
+
+            customApi.selectById(cid)
+                .then(resp => {
+                    customUpdate.value = resp.data;
+                    console.log(customUpdate);
+                    familyApi.selectById(resp.data.fid)
+                        .then(resp => [
+                            familyUpdate.value = resp.data,
+                            updateDialogShow.value = true,
+                        ])
+                })
+        })
+}
+
 
 //删除家属
 function deleteFamily(fid) {
     familyApi.delete(fid)
         .then(resp => {
             console.log(resp);
-            
+
             if (resp.code == 10000) {
                 ElMessage.success(resp.msg);
                 selectByPage(1)
@@ -78,6 +151,7 @@ function deleteFamily(fid) {
 }
 
 selectByPage(1);
+selectByPage2(1);
 </script>
 
 <style scoped></style>
